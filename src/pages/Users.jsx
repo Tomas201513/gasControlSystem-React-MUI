2364aa6c27f4add96dd714153d15b197b7a1e683
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2023-04-29 09:05:30 +0300

    Update Users.jsx

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Box } from '@mui/system'
+import React from 'react';
+import { Box } from '@mui/system';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
 function Users() {
+  const sampleData = [
+  { id: 1, name: 'John Doe', age: 30, city: 'New York' },
+  { id: 2, name: 'Jane Smith', age: 25, city: 'Los Angeles' },
+  { id: 3, name: 'Michael Johnson', age: 45, city: 'Chicago' },
+];
   return (
-   <Box sx={{ p: 3 , height: '100vh', background: 'linear-gradient(180deg, #FFFFFF 0%, #F8F8F8 100%)'}}>
-            <h1>
-            users
-            </h1> 
-            </Box>
+ <>
+  <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Age</TableCell>
+            <TableCell>City</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sampleData.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell>{row.id}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.age}</TableCell>
+              <TableCell>{row.city}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+ 
+ </>
 
   )
 }
